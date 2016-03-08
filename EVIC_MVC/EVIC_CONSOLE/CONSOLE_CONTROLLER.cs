09261de960a9261c83afd8c1eba7544b5a961c04
Luis Bobadilla <superluis5@gmail.com>
2016-03-08 15:22:24 -0600

    initial

---
+++ EVIC_MVC/EVIC_CONSOLE/CONSOLE_CONTROLLER.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVIC_MVC
+{
+    class CONSOLE_CONTROLLER
+    {
+        static void Main(string[] args)
+        {
+            //creation of view object
+            CONSOLE_VIEW _view = new CONSOLE_VIEW();
+
+            //creation of model object
+            EVIC_Model _model = new EVIC_Model();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ EVIC_MVC/EVIC_MVC/EVIC_Model.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVIC_MVC
+{
+    public class EVIC_Model
+    {
+        /// <summary>
+        /// Field for the odometer
+        /// </summary>
+        private static int _odometer = 0;
+
+        /// <summary>
+        /// Get/Set for odometer
+        /// </summary>
+        public int Odometer
+        {
+            get { return _odometer; }
+            set { _odometer = value; }
+        }
+        
+        /// <summary>
+        /// Field for miles until oil change
+        /// </summary>
+        private static int _milesToOilChange = 3000;
+
+        /// <summary>
+        /// Get/Set for miles until oil change
+        /// </summary>
+        public int MilesToOilChange
+        {
+            get { return _milesToOilChange; }
+            set { _milesToOilChange = value;  }
+        }
+
+        /// <summary>
+        /// Field for checking which mode we are in
+        /// </summary>
+        private static bool _isMetric = false;
+
+        /// <summary>
+        /// Get/Set for isMetric field
+        /// </summary>
+        public bool isMetric
+        {
+            get { return _isMetric; }
+            set { _isMetric = value; }
+        }
+        
+        /// <summary>
+        /// field for the toggled condition for the probram
+        /// </summary>
+        private static bool _isToggled = false;
+        /// <summary>
+        /// get/set for isToggled field
+        /// </summary>
+        public bool isToggled
+        {
+            get { return _isMetric; }
+            set { _isMetric = value; }
+        }
+
+
+
+
+
+
+
+
+   
+        
+
+
+
+
+    }
+}
