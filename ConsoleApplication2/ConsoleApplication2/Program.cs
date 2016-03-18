a06cea5a4acf8eb2a725ee80a011fb64164e2a99
Luis Bobadilla <superluis5@gmail.com>
2016-03-17 21:03:50 -0500

    app for csv to txt tart

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter file name");
+            string answer = Console.ReadLine();
+
+            char delim = ',';
+
+            StreamReader sr = new StreamReader(answer);
+            Console.WriteLine("enter destination file name");
+            string fileoutput = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(fileoutput);
+
+            string[] words = new string[10000000];
+
+            string line;
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+                words = line.Split(delim);
+
+
+
+                foreach (string s in words)
+                {
+
+                    if (!s.Contains('2'))
+                    {
+                        sw.WriteLine(s);
+                    }
+                    else if (!s.Contains('@'))
+                    {
+                        sw.WriteLine(s);
+                    }
+
+                }
+            }
+
+
+                sr.Close();
+
+
+
+                sw.Close();
+
+
+                Console.ReadLine(); // so the program doesn't stop on its own
+
+
+          
+        }
+    }
+}
